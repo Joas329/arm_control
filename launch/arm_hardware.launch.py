032d122c61d5624502e4c7f181be06bd71b68752
Joas329 <joaquinphilco@hotmail.com>
2025-04-22 14:30:23 -0400

    hardware launch: Use new template to launch using ros2 controllers and parameters of the new urdf file.
    Signed-off-by: Joas329 <joaquinphilco@hotmail.com>

--- launch/arm_hardware.launch.py
+++ launch/arm_hardware.launch.py
@@ -1,15 +1,12 @@
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument, TimerAction
-from launch.conditions import IfCondition, UnlessCondition
-from launch.event_handlers import OnProcessExit
-from moveit_configs_utils import MoveItConfigsBuilder
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-import xacro
-import yaml
+from launch_ros.parameter_descriptions import ParameterFile
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 
 
 '''
@@ -30,15 +27,44 @@ The following architechture is:
 package_name='arm_control'
 
 def generate_launch_description():
+    serial_port = LaunchConfiguration("serial_port")
+    calibrate = LaunchConfiguration("calibrate")
+    include_gripper = LaunchConfiguration("include_gripper")
+    arduino_serial_port = LaunchConfiguration("arduino_serial_port")
+    arm_model_config = LaunchConfiguration("arm_model")
+    tf_prefix = LaunchConfiguration("tf_prefix")
 
     print("Launching arm part: ")
 
     # ****************** Robot Description ****************** #
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name="xacro")]),
+        " ",
+        PathJoinSubstitution(
+            [FindPackageShare("arm_control"), "description", "hardware_launch.urdf.xacro"]),
+        " ",
+        "arm_model:=",
+        arm_model_config,
+        " ",
+        "serial_port:=",
+        serial_port,
+        " ",
+        "calibrate:=",
+        calibrate,
+        " ",
+        "tf_prefix:=",
+        tf_prefix,
+        " ",
+        "include_gripper:=",
+        include_gripper,
+        " ",
+        "arduino_serial_port:=",
+        arduino_serial_port,
+    ])
 
-    pkg_path = os.path.join(get_package_share_directory(package_name))
-    xacro_file = os.path.join(pkg_path,'description','hardware_launch.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_description = {'robot_description': robot_description_config.toxml()}
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_content, value_type=str)
+    }
 
     # ****************** Robot State Publisher (joint_states) ****************** #
 
@@ -50,20 +76,39 @@ def generate_launch_description():
     )
 
     # ****************** ROS2 Controllers (ros2 controllers) ****************** #
-
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','arm_controllers.yaml')
-    control_node = Node(
+    controller_manager_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, controller_params_file],
-        output="both",
+        parameters=[
+            update_rate_config_file,
+            ParameterFile(joint_controllers_cfg, allow_substs=True),
+            {
+                "tf_prefix": tf_prefix
+            },
+        ],
+        remappings=[('~/robot_description', 'robot_description')],
+        output="screen",
     )
 
-    arm_controller_spawner = Node(
+    joint_controllers_cfg = PathJoinSubstitution(
+        [FindPackageShare("arm_control"), "config", "arm_controllers.yaml"])
+
+    update_rate_config_file = PathJoinSubstitution([
+        FindPackageShare("annin_ar4_driver"),
+        "config",
+        "controller_update_rate.yaml",
+    ])
+
+    arm_controller_spawner =  Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["arm_group_controller", "--controller-manager", "/controller_manager"],
-        output="both",
+        arguments=[
+            "joint_trajectory_controller",
+            "-c",
+            "/controller_manager",
+            "--controller-manager-timeout",
+            "100",
+        ],
     )
 
     # ****************** Joint State Broadcaster ****************** #
@@ -115,14 +160,55 @@ def generate_launch_description():
         output="both",
     )
 
-    return LaunchDescription([
-        robot_state_pub_node,
-        control_node,
-        joint_state_broadcaster_spawner,
-        arm_controller_spawner,
-        joint_encoder_spawner,
-        joint_decoder_spawner,
-        udp_receiver,
-        udp_transmitter,
-        PID_scripts_spawner
-    ])
+    # ****************** Launch Description ****************** #
+    ld = LaunchDescription()
+    ld.add_action(
+        DeclareLaunchArgument(
+            "serial_port",
+            default_value="/dev/ttyACM0",
+            description="Serial port to connect to the robot",
+        ))
+    ld.add_action(
+        DeclareLaunchArgument(
+            "calibrate",
+            default_value="False",
+            description="Calibrate the robot on startup",
+            choices=["True", "False"],
+        ))
+    ld.add_action(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="",
+            description="Prefix for AR4 tf_tree",
+        ))
+    ld.add_action(
+        DeclareLaunchArgument(
+            "include_gripper",
+            default_value="True",
+            description="Run the servo gripper",
+            choices=["True", "False"],
+        ))
+    ld.add_action(
+        DeclareLaunchArgument(
+            "arduino_serial_port",
+            default_value="/dev/ttyUSB0",
+            description="Serial port of the Arduino nano for the servo gripper",
+        ))
+    ld.add_action(
+        DeclareLaunchArgument("arm_model",
+                              default_value="mk3",
+                              choices=["mk1", "mk2", "mk3"],
+                              description="Model of AR4"))
+    ld.add_action(controller_manager_node)
+    ld.add_action(arm_controller_spawner)
+    # ld.add_action(gripper_controller_spawner)
+    ld.add_action(robot_state_pub_node)
+    ld.add_action(joint_state_broadcaster_spawner)
+    ld.add_action(joint_encoder_spawner)
+    ld.add_action(joint_decoder_spawner)
+    ld.add_action(joint_decoder_spawner)
+    ld.add_action(udp_receiver)
+    ld.add_action(udp_transmitter)
+    ld.add_action(PID_scripts_spawner)
+
+    return ld
